2b9c47c6252856b1cfca9d5849a0fe76c4cb8e90
MikuroXina <ryosukadnak@gmail.com>
2019-12-18 23:40:24 +0900

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare function doFileSync(file_name: string): number;
-declare function doFile(file_name: string): Promise<number>;
+declare function doFile(file_name: string): Promise<void>;
 declare function doStringSync(program: string): number;
-declare function doString(program: string): Promise<number>;
-declare function loadProgram(program: string): LuaProgram;
-declare class LuaProgram {
+declare function doString(program: string): Promise<void>;
+declare function loadProgram(program: string): State;
+declare class State {
   setTable(table: object): void;
   run(): Promise<object>;
 }
