55ca34f05e6a56fe67813f791a37a0a740ab1f6f
MikuroXina <ryosukadnak@gmail.com>
2021-11-17 15:00:08 +0900

    Extract into convert_err

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,25 @@
-use mlua::Lua;
+use mlua::{Error, Lua};
 use neon::prelude::*;
+
+fn convert_err<'a>(
+    res: mlua::Result<()>,
+    cx: &mut FunctionContext<'a>,
+) -> JsResult<'a, JsUndefined> {
+    res.map(|_| cx.undefined()).or_else(|err| {
+        if let Error::SyntaxError { message, .. } = err {
+            cx.throw_type_error(message)
+        } else {
+            cx.throw_error(format!("lua exec failed: {:?}", err))
+        }
+    })
+}
 
 fn do_string_sync(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let lua = Lua::new();
-    lua.load(&cx.argument::<JsString>(0)?.value(&mut cx))
-        .exec()
-        .or_else(|err| cx.throw_error(format!("lua exec failed: {:?}", err)))?;
-    Ok(cx.undefined())
+    convert_err(
+        lua.load(&cx.argument::<JsString>(0)?.value(&mut cx)).exec(),
+        &mut cx,
+    )
 }
 
 #[neon::main]
