7317ed26d44feff951e43353c11a2fc2f6410fec
MikuroXina <ryosukadnak@gmail.com>
2021-11-24 10:59:49 +0900

    Use lua crate

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,27 +1,31 @@
-use mlua::{Error, Lua};
+use lua::{State, ThreadStatus};
 use neon::prelude::*;
 
-mod convert;
-
-use convert::lua_to_js;
-
-fn convert_err<'j, T>(res: Result<T, Error>, cx: &mut impl Context<'j>) -> NeonResult<T> {
-    res.or_else(|err| {
-        if let Error::SyntaxError { message, .. } = err {
-            cx.throw_type_error(message)
-        } else {
-            cx.throw_error(format!("lua exec failed: {:?}", err))
-        }
-    })
+fn convert_err<'j>(
+    status: ThreadStatus,
+    state: &mut State,
+    cx: &mut impl Context<'j>,
+) -> JsResult<'j, JsValue> {
+    if let ThreadStatus::Ok = status {
+        return Ok(cx.undefined().as_value(cx));
+    }
+    let err = state
+        .to_str(-1)
+        .expect("error message not found")
+        .to_owned();
+    if let ThreadStatus::SyntaxError = status {
+        cx.throw_type_error(err)
+    } else {
+        cx.throw_error(format!("lua exec failed: {:?}", err))
+    }
 }
 
 fn do_string_sync(mut cx: FunctionContext) -> JsResult<JsValue> {
-    let lua = Lua::new();
-    convert_err(
-        lua.load(&cx.argument::<JsString>(0)?.value(&mut cx)).eval(),
-        &mut cx,
-    )
-    .and_then(|lua| lua_to_js(lua, &mut cx))
+    let program = cx.argument::<JsString>(0)?.value(&mut cx);
+    let mut state = State::new();
+    state.open_libs();
+    let status = state.do_string(&program);
+    convert_err(status, &mut state, &mut cx)
 }
 
 #[neon::main]
