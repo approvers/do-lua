7b267d75994a3404caf2ac2b4c5e265a8ac9d0b0
MikuroXina <ryosukadnak@gmail.com>
2021-11-24 11:23:39 +0900

    Add doString

--- native/src/do_string.rs
+++ native/src/do_string.rs
@@ -10,3 +10,26 @@ pub fn do_string_sync(mut cx: FunctionContext) -> JsResult<JsValue> {
     let status = state.do_string(&program);
     convert_err(status, &mut state, &mut cx)
 }
+
+pub fn do_string_async(mut cx: FunctionContext) -> JsResult<JsValue> {
+    let program = cx.argument::<JsString>(0)?.value(&mut cx);
+    let callback = cx.argument::<JsFunction>(1)?.root(&mut cx);
+    let channel = cx.channel();
+
+    std::thread::spawn(move || {
+        let mut state = State::new();
+        state.open_libs();
+        let status = state.do_string(&program);
+
+        channel.send(move |mut cx| {
+            let callback = callback.into_inner(&mut cx);
+            let this = cx.undefined();
+            let args = vec![convert_err(status, &mut state, &mut cx)?];
+
+            callback.call(&mut cx, this, args)?;
+
+            Ok(())
+        });
+    });
+    Ok(cx.undefined().as_value(&mut cx))
+}
