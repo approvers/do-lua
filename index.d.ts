0f3f143afdd8b8ee4a2f345f900f006ca0d3d8e3
MikuroXina <ryosukadnak@gmail.com>
2021-12-01 10:53:32 +0900

    Fix typing

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ declare function doFile(file_name: string): Promise<void>;
 declare function doStringSync(program: string): void;
 declare function doString(program: string): Promise<void>;
 declare class State {
-  new(program: string): State;
+  constructor(program: string);
   setTable(name: string, table: Record<string, unknown>): void;
   run(): Promise<Record<string, unknown>>;
 }
